31a34416f84ae495111188f25e4b98b84f49066b
august sletto <augustsletto@gmail.com>
2024-03-26 20:34:25 +0000

    init backend

---
+++ backend/base/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from .products import products
+
+def getRoutes(request):
+    routes = [
+        '/api/products/',
+        '/api/products/create/',
+
+        '/api/products/upload',
+
+        '/api/products/<id>/reviews/',
+
+        '/api/products/top/',
+        '/api/products/<id>/',
+
+        '/api/products/delete/<id>',
+        '/api/products/<update>/<id>',
+
+        
+
+    ]
+
+    return JsonResponse('Hello', safe=False)
+
+def getProducts(request):
+    return JsonResponse(products, safe=False)
