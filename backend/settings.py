0afb72c2805381868c6158fafc91bc8ff3323e21
august sletto <augustsletto@gmail.com>
2024-03-31 13:52:52 +0000

    test

--- backend/settings.py
+++ backend/settings.py
@@ -47,7 +47,7 @@ ALLOWED_HOSTS = [
 
 
 
-# CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 
@@ -151,6 +151,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 
